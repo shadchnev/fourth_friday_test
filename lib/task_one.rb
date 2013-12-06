ecac7048b29980d6e09f82de7f28914bf7682ddc
James Brooke <james@tnwo.co>
2013-12-06 11:21:04 +0000

    Array#injection should be able to multiply the values of [1, 2, 3, 4]

--- lib/task_one.rb
+++ lib/task_one.rb
@@ -1,7 +1,8 @@
 class Array
   def injection(*args, &block)
     result = args.first || self.first
-    self.each do |element|
+    self.each_with_index do |element, index|
+      next if args.empty? && index == 0
       result = yield(result, element)
     end
     result
