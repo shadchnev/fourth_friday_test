2fac5301ec59cc4cdf629a1dd4502446b362bc4d
James Brooke <james@tnwo.co>
2013-12-06 14:51:15 +0000

    using money gem rather than strings

--- lib/task_two.rb
+++ lib/task_two.rb
@@ -1,8 +1,12 @@
+require 'money'
+
 class Takeaway
-  MENU = { :Bliny => "£0.45", :Caviar => "£1000.00", :Kasha => "£1.00", :Knish => "£0.55" }
+  MENU = { :Bliny => Money.new(45, "GBP"), :Caviar => Money.new(100_000, "GBP"), :Kasha => Money.new(100, "GBP"), :Knish => Money.new(55, "GBP")}
   def self.list
     MENU.each do |dish, price|
-      puts "#{dish}\t#{price}"
+      puts "#{dish}\t£#{price}"
     end
   end
+  # def initialize(options = {})
+  #   defaults = { :Bliny =>}
 end
