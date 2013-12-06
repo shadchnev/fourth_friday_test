67d42b123b13f3bf2d4f287b1730b660d9b66ca8
James Brooke <james@tnwo.co>
2013-12-06 12:13:19 +0000

    Some refactoring complete

--- lib/task_one.rb
+++ lib/task_one.rb
@@ -1,25 +1,18 @@
 class Array
   def injection(*args)
-    if args.size == 1 && !block_given?
-      result = self.first
-      my_proc = proc { |result, element| result = result.send(args.first, element) }
-      self.each_with_index do |element, index|
-        next if index == 0
-        result = my_proc.call(result, element)
-      end
-      result
-    elsif args.size == 2
-      result = args.first
-      my_proc = proc { |result, element| result = result.send(args.last, element) }
-      self.each_with_index do |element, index|
-        result = my_proc.call(result, element)
-      end
-      result
-    else
+    if block_given?
       result = args.first || self.first
       self.each_with_index do |element, index|
         next if args.empty? && index == 0
         result = yield(result, element)
+      end
+      result
+    else
+      result = args.size == 1 ? self.first : args.first
+      my_proc = proc { |result, element| result = result.send(args.last, element) }
+      self.each_with_index do |element, index|
+        next if index == 0 && args.size == 1
+        result = my_proc.call(result, element)
       end
       result
     end
