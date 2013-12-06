7fd4175cb4596b2d8f0d2d4ca19eda6f96fd87d6
James Brooke <james@tnwo.co>
2013-12-06 12:41:22 +0000

    deliberately creating duplication to be able to refactor

--- lib/task_one.rb
+++ lib/task_one.rb
@@ -1,15 +1,16 @@
 class Array
-  def injection(*args)
+  def injection(*args, &block)
     if block_given?
       result = args.first || self.first
+      my_proc = block_given? ? block : proc { |result, element| result = result.send(args.last, element) }
       self.each_with_index do |element, index|
-        next if args.empty? && index == 0
-        result = yield(result, element)
+        next if index == 0 && args.size == 0
+        result = my_proc.call(result, element)
       end
       result
     else
       result = args.size == 1 ? self.first : args.first
-      my_proc = proc { |result, element| result = result.send(args.last, element) }
+      my_proc = block_given? ? block : proc { |result, element| result = result.send(args.last, element) }
       self.each_with_index do |element, index|
         next if index == 0 && args.size == 1
         result = my_proc.call(result, element)
